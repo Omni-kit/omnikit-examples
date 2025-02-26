a51c338948e9be756ef0b8cd47276ff9f8b81b4b
0xBhumi <bhumisadariya@lampros.tech>
2025-02-26 14:55:18 +0530

    feat: test script added for state synchronization example

--- test/StateSync/TestCounter.t.sol
+++ test/StateSync/TestCounter.t.sol
@@ -13,20 +13,26 @@ contract TestCounter is Test {
     error CallerNotL2ToL2CrossDomainMessenger();
     error InvalidCrossDomainSender();
 
+    // Fork IDs for chain A and chain B
     uint256 forkA;
     uint256 forkB;
+
+    // Contract deployers and contract addresses
     ContractDeployer deployer901;
     ContractDeployer deployer902;
     address primaryCounter901;
     address counter902;
 
+    // Setup function to initialize the test environment
     function setUp() public {
+        // Salt for deterministic deployment
         bytes32 salt = keccak256(abi.encodePacked("counter"));
 
         // Create forks for chain A and chain B
         forkA = vm.createFork("http://127.0.0.1:9545"); // chainId 901
         forkB = vm.createFork("http://127.0.0.1:9546"); // chainId 902
 
+        // Deploy PrimaryCounter on chain A
         vm.selectFork(forkA);
         deployer901 = new ContractDeployer{salt: "deployer"}();
         primaryCounter901 = deployer901.deploy(
@@ -34,13 +40,62 @@ contract TestCounter is Test {
             type(PrimaryCounter).creationCode
         );
 
+        // Deploy Counter on chain B
         vm.selectFork(forkB);
         deployer902 = new ContractDeployer{salt: "deployer"}();
         counter902 = deployer902.deploy(salt, type(Counter).creationCode);
     }
 
-    function testDeployment() public view {
-        console.log("primary counter...", primaryCounter901);
-        console.log("counter...", counter902);
+    // Test function to verify state synchronization between chains
+    function testSyncStatesFunction() public {
+        vm.selectFork(forkA);
+
+        PrimaryCounter(primaryCounter901).setNumber(2);
+
+        uint256 number901 = PrimaryCounter(primaryCounter901).number();
+        console.log("number on 901 chain", number901);
+
+        vm.selectFork(forkB);
+
+        address messengerAddr = Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER;
+        vm.prank(messengerAddr);
+
+        // Mock the cross-domain messenger to simulate a cross-chain message
+        vm.mockCall(
+            messengerAddr,
+            abi.encodeWithSelector(
+                IL2ToL2CrossDomainMessenger.crossDomainMessageSender.selector
+            ),
+            abi.encode(counter902)
+        );
+        Counter(counter902).setNumber(2);
+
+        uint256 number902 = Counter(counter902).number();
+        console.log("number on 902 chain", number902);
+    }
+
+    // Test function to ensure only the cross-domain messenger can call the setNumber function
+    function testOnlyCrossDomainCallbackCanCall() public {
+        vm.selectFork(forkB);
+
+        // Direct call fails
+        vm.expectRevert(CallerNotL2ToL2CrossDomainMessenger.selector);
+        Counter(counter902).setNumber(2);
+
+        // Mock the cross-domain messenger to simulate a cross-chain message
+        address messengerAddr = Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER;
+        vm.prank(messengerAddr);
+        vm.mockCall(
+            messengerAddr,
+            abi.encodeWithSelector(
+                IL2ToL2CrossDomainMessenger.crossDomainMessageSender.selector
+            ),
+            abi.encode(address(0xdead))
+        );
+
+        vm.expectRevert(InvalidCrossDomainSender.selector);
+
+        Counter(counter902).setNumber(2);
+        vm.stopPrank();
     }
 }
